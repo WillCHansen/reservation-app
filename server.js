db5ac6b8ffb85203207f1318b8363424aaa7baaf
William Hansen <cade.hansen@gmail.com>
2017-04-15 11:46:51 -0700

    added api route

--- server.js
+++ server.js
@@ -17,14 +17,24 @@ app.get("/view", function(req, res) {
   res.sendFile(path.join(__dirname, "view.html"));
 });
 
+app.get("/api/tables", function(req, res) {
+	return res.json(tables)
+});
 
-
-
-
-
-
-
-
+var tables = [
+	{
+		name: "david",
+		phone: 911,
+		email: "sekc@gmail",
+		id: 1
+	},
+	{
+		name: "Blake",
+		phone: 912,
+		email: "Blake@gmail",
+		id: 2
+	}
+];
 
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
--- tableCreator.js
+++
@@ -1,8 +0,0 @@
-var TableCreator = function(name, phone, email, id) {
-	this.name = name;
-	this.phone = phone;
-	this.email = email;
-	this.id = id;
-}
-
-module.exports = TableCreator;

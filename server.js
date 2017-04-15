07cbe5c2be4a627a30339186dcd9c683718bd932
David Naimi <d88naimi@gmail.com>
2017-04-15 11:26:19 -0700

    added links

--- server.js
+++ server.js
@@ -4,6 +4,28 @@ var path = require("path");
 var app = express();
 var PORT = 3000;
 
+
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "home.html"));
+});
+
+app.get("/make", function(req, res) {
+  res.sendFile(path.join(__dirname, "make.html"));
+});
+
+app.get("/view", function(req, res) {
+  res.sendFile(path.join(__dirname, "view.html"));
+});
+
+
+
+
+
+
+
+
+
+
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
 });
